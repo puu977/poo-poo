a0410be54579137e8897039c07efef3245bdee91
Puu <66482734+puu977@users.noreply.github.com>
2026-06-21 13:00:37 +0700

    Delete comp-js-create.js

--- comp-js-create.js
+++
@@ -1,3 +0,0 @@
-let data={text:'',media:null,music:null,loc:'',feeling:'',tags:[]};
-const txt=document.getElementById('txt'),send=document.getElementById('send');
-txt.oninput=()=>{data.text=txt.value;send.disabled=!data.text.trim()&&!data.media};
