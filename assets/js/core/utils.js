b072ac3997228ed3c122d30d24ed7650f520b322
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 00:54:16 +0700

    Create assets/js/core/utils.js

---
+++ assets/js/core/utils.js
@@ -0,0 +1,12 @@
+export function formatTime(date){
+
+  return new Date(date)
+    .toLocaleTimeString(
+      'id-ID',
+      {
+        hour:'2-digit',
+        minute:'2-digit'
+      }
+    );
+
+}
