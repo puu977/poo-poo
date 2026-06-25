34e08cf3bc2c51a80ae29cdb864b5a445eec3fba
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 00:37:38 +0700

    Create assets/js/views/home.js

---
+++ assets/js/views/home.js
@@ -0,0 +1,29 @@
+export function renderHome(){
+
+document.getElementById('view-home').innerHTML=`
+
+<div class="home">
+
+<h1>Home</h1>
+
+<div class="stories">
+
+Story 1
+
+Story 2
+
+Story 3
+
+</div>
+
+<div class="feed">
+
+Postingan muncul di sini
+
+</div>
+
+</div>
+
+`;
+
+}
