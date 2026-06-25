7cb29365be219fc3cac441ecb362979532322219
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 00:38:06 +0700

    Create assets/js/views/chat.js

---
+++ assets/js/views/chat.js
@@ -0,0 +1,23 @@
+export function renderChat(){
+
+document.getElementById('view-chat').innerHTML=`
+
+<div class="chat-layout">
+
+<div class="chat-left">
+
+Kontak
+
+</div>
+
+<div class="chat-right">
+
+Pesan
+
+</div>
+
+</div>
+
+`;
+
+}
