b74dd3ccf469de189945ae0061e385f8cf9b7931
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 21:50:22 +0700

    Refactor HomeView to use state management
    Refactor HomeView function to integrate state management and simplify structure.

--- views/home/index.js
+++ views/home/index.js
@@ -1,24 +1,3 @@
-import { Navbar } from '../../components/topbar.js';
-import { Sidebar } from '../../components/sidebar.js';
-import { Feed } from './feed.js';
-
-export async function HomeView(){
-  const app = document.getElementById('app');
-  app.innerHTML = '';
-  
-  app.appendChild(Navbar());
-  
-  const bottomSec = document.createElement('div');
-  bottomSec.className = 'bottom-sec';
-  bottomSec.appendChild(Sidebar());
-  
-  const chatSection = document.createElement('div');
-  chatSection.className = 'chat-section';
-  chatSection.appendChild(await Feed());
-  bottomSec.appendChild(chatSection);
-  
-  app.appendChild(bottomSec);
-}
 import { state } from '../../core/state.js';
 
 export async function HomeView(){
