04aa90ee309938b19b0ce33664f96d3cc6f04b2b
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 22:47:17 +0700

    Delete components directory

--- components/buttons/like.js
+++
@@ -1,8 +0,0 @@
-import { supa } from '../../core/api.js';
-import { state } from '../../core/state.js';
-
-export function LikeButton(post) {
-  const liked = state.myLikes.has(post.id);
-  const btn = document.createElement('button');
-  btn.className = 'like-btn';
- 
