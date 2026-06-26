22b6e4280c1b5566b478ae36efbfb98dbe819adf
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 22:47:40 +0700

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,5 +0,0 @@
-import { HomeView } from './views/home/index.js';
-import { state } from './core/state.js';
-window.state = state;
-
-document.addEventListener('DOMContentLoaded', HomeView);
--- assets/js/core/state.js
+++
@@ -1,7 +0,0 @@
-export const state = {
-  user: {
-    id: 'test',
-    username: 'Anonim',
-    avatar: 'assets/img/avatar-blank.png'
-  }
-};
