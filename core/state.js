4fc0d8d99bfd6f4a17ae3f0c145b9dd356335893
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 20:56:12 +0700

    Fix formatting and ensure newline at end of file

--- core/state.js
+++ core/state.js
@@ -2,7 +2,7 @@ export const state = {
   user: {
     id: localStorage.puu_id || (localStorage.puu_id='u_'+Math.random().toString(36).slice(2,9)),
     username: 'Anonim',
-    avatar: 'assets/img/avatar-blank.png' // default blank
+    avatar: 'assets/img/avatar-blank.png'
   },
   posts: [],
   myLikes: new Set()
