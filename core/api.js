806f332e95cf5786c37d22276418ff769198c431
Puu <66482734+puu977@users.noreply.github.com>
2026-06-26 21:06:49 +0700

    Delete core directory

--- core/api.js
+++
@@ -1,2 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-export const supa = createClient('https://gttedpjwqgiyszmrcnoe.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0dGVkcGp3cWdpeXN6bXJjbm9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzNjk3MjIsImV4cCI6MjA5Njk0NTcyMn0.GzouEkyd6pRvT_IQuNR7LTyN6ewPiCTj2Xqu8l9yEcY');
--- core/state.js
+++
@@ -1,9 +0,0 @@
-export const state = {
-  user: {
-    id: localStorage.puu_id || (localStorage.puu_id='u_'+Math.random().toString(36).slice(2,9)),
-    username: 'Anonim',
-    avatar: 'assets/img/avatar-blank.png'
-  },
-  posts: [],
-  myLikes: new Set()
-};
